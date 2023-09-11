f95db4168d57e5200dd20c2cf3e46f186c2f2252
Di <dierli.muller@gmail.com>
2023-09-11 20:17:19 +0100

    envio projeto finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
+const pokemonDetails = document.querySelector("#modal .pokemon-details")
+
 const maxRecords = 151
-const limit = 10
+const limit = 5
 let offset = 0;
+
+
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -23,10 +27,25 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
-function loadPokemonItens(offset, limit) {
+function loadPokemonItens(offset, limit){
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+      const newHtml = pokemons.map((pokemon)=>`
+          <li class="pokemon ${pokemon.type}" onclick="openModal(${pokemon.number})">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                  <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                  </ol>
+                  <img
+                    src="${pokemon.photo}"
+                    alt="${pokemon.name}"
+                    
+                  />
+                </div>
+          </li>
+      `).join('')
+      pokemonList.innerHTML += newHtml
     })
 }
 
@@ -45,3 +64,9 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+const reloadButton = document.getElementById('reloadButton');
+
+reloadButton.addEventListener('click', () => {
+    location.reload();
+});
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,12 @@
 
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
-}
+    number
+    name
+    type
+    types = []
+    photo
+    stats = []
+    totalStats
+    abilities = []
+    ability
+  }
